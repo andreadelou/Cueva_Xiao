7610c8b512d4992e21841f7900faf92acf4ddf42
andreadelou <lam20102@uvg.edu.gt>
2022-04-27 11:42:38 -0600

    Juego

---
+++ src/goal.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import {b} from './laberinto.jsx'
+import img from './goal.png'
+const Goal = ({x,y}) =>{
+
+    const style = {
+        width: `${b}px`,
+        height: `${b}px`,
+        background: 'orange',
+        gridColumnStart: x+1,
+        gridRowStart: y+1
+    }
+    return (
+        <img src={img} style={style}/>
+    )
+}
+
+export default Goal
---
+++ src/pared.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import {b} from './laberinto.jsx'
+import img from './arbusto1.jpg'
+const Pared = ({x,y}) =>{
+
+    const style = {
+        width: `${b}px`,
+        height: `${b}px`,
+        background: 'black',
+        gridColumnStart: x+1,
+        gridRowStart: y+1
+    }
+    return (
+        <img src={img} style={style}/>
+    )
+}
+
+export default Pared
